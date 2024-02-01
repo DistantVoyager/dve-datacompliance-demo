965cd7aafa89e5275641bdc58d4077da47718450
Steven Weiblinger <111259311+DistantVoyager@users.noreply.github.com>
2024-02-01 11:12:39 -0500

    Update my_data_compliance_rules.py

--- custom_validators/my_data_compliance_rules.py
+++ custom_validators/my_data_compliance_rules.py
@@ -10,14 +10,9 @@ class DeviceDataComplianceRules(DataComplianceRule):
         if not re.match("^[a-zA-Z0-9\-_.]+$", self.context["object"].name):
             raise ComplianceError({"name": "Device name contains unallowed special characters."})
     
-    # Checks if a device is not assigned to a rack
-    def audit_device_rack(self):
-        if not self.context["object"].rack:
-            raise ComplianceError({"rack": "Device should be assigned to a rack."})
-    
     def audit(self):
         messages = {}
-        for fn in [self.audit_device_name_chars, self.audit_device_rack]:
+        for fn in [self.audit_device_name_chars]:
             try:
                 fn()
             except ComplianceError as ex:
@@ -25,3 +20,21 @@ class DeviceDataComplianceRules(DataComplianceRule):
         if messages:
             raise ComplianceError(messages)
 
+class DeviceDataComplianceRules(DataComplianceRule):
+    model = "dcim.device"
+    enforce = False
+    
+    # Checks if a device is not assigned to a rack
+    def audit_device_rack(self):
+        if not self.context["object"].rack:
+            raise ComplianceError({"rack": "Device should be assigned to a rack."})
+    
+    def audit(self):
+        messages = {}
+        for fn in [self.audit_device_rack]:
+            try:
+                fn()
+            except ComplianceError as ex:
+                messages.update(ex.message_dict)
+        if messages:
+            raise ComplianceError(messages)
